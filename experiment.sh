54a2ce4e4fcfec5d8f4912d1b0f048bdb1c9d145
Théo <gustintheo@protonmail.com>
2022-12-04 20:21:20 +0100

    bashtime

--- experiment.sh
+++ experiment.sh
@@ -3,16 +3,16 @@ echo "Program;Threads;Test;Temps"
 
 for i in 1 2 4 8 16 32 64;do
     for j in $(seq 1 5);do
-        echo "Philosof;$i;$j;$(/usr/bin/time temp/Philosof $i --quiet 2>&1 | cut -b 1,2,3,4 | head -1)"
+        echo "Philosof;$i;$j;$(/usr/bin/time -f %e temp/Philosof $i --quiet 2>&1)"
     done
 done
 for i in 1 2 4 8 16 32 64;do
     for j in $(seq 1 5);do
-        echo "Producer_Consumer;$i;$j;$(/usr/bin/time temp/Producer_Consumer $i $i --quiet 2>&1 | cut -b 1,2,3,4 | head -1)"
+        echo "Producer_Consumer;$i;$j;$(/usr/bin/time -f %e temp/Producer_Consumer $i $i --quiet 2>&1)"
     done
 done
 for i in 1 2 4 8 16 32 64;do
     for j in $(seq 1 5);do
-        echo "WriterReader;$i;$j;$(/usr/bin/time temp/Producer_Consumer $i $i --quiet 2>&1 | cut -b 1,2,3,4 | head -1)"
+        echo "WriterReader;$i;$j;$(/usr/bin/time -f %e temp/Producer_Consumer $i $i --quiet 2>&1)"
     done
 done
