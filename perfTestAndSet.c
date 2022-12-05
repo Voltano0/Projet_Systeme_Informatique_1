8169db1b4334a6a35fc337030a334ad6c8be7679
Théo <gustintheo@protonmail.com>
2022-12-05 16:48:41 +0100

    sa marche pas sa mère

--- perfTestAndSet.c
+++ perfTestAndSet.c
@@ -1,1 +1,31 @@
-coucou je suis un nouveau fichier c
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+int lockAllThread = 0;
+
+void lock(){
+    int inter =0;
+    asm(
+        "movl $1, %%eax;"
+        "xchgl %%eax %1;"
+        "movl %%eax, %0;" 
+        :"=r"(inter, lockAllThread)        //Output
+        :"r"(lockAllThread)                 //Input
+        :"%eax","%ebx"
+        );
+    printf("%d", inter);
+    printf("%d", lockAllThread);
+};
+
+void unlock();
+
+
+int main(int argc, char const *argv[])
+{
+    int N = atoi(argv[1]);
+    lock();
+    return 0;
+}
