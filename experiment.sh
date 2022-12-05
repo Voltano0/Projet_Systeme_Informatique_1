221441e39490d02267fa656cf3b71a409e45aa48
Théo <gustintheo@protonmail.com>
2022-12-05 15:28:17 +0100

    b

--- experiment.sh
+++ experiment.sh
@@ -2,17 +2,17 @@
 echo "Program;Threads;Test;Temps"
 
 for i in 1 2 4 8 16 32 64;do
-    for j in $(seq 1 20);do
+    for j in $(seq 1 5);do
         echo "Philosof;$i;$j;$(/usr/bin/time -f %e temp/Philosof $i --quiet 2>&1)"
     done
 done
 for i in 1 2 4 8 16 32 64;do
-    for j in $(seq 1 20);do
+    for j in $(seq 1 5);do
         echo "Producer_Consumer;$i;$j;$(/usr/bin/time -f %e temp/Producer_Consumer $i $i --quiet 2>&1)"
     done
 done
 for i in 1 2 4 8 16 32 64;do
-    for j in $(seq 1 20);do
+    for j in $(seq 1 5);do
         echo "WriterReader;$i;$j;$(/usr/bin/time -f %e temp/WriterReader $i $i --quiet 2>&1)"
     done
 done
