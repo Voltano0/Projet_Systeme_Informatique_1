42fe14c07a3f550af7af64b6dd0825fda5d728ae
GigaUltimatumXVI <adriennotte@hotmail.com>
2022-12-05 17:11:48 +0100

    merging

--- perfTestAndSet.c
+++ perfTestAndSet.c
@@ -7,31 +7,25 @@
 int lockAllThread = 0;
 
 void lock(){
-    int inter =0;
     //implement spinlock with inline assempbly
-    __asm__ __volatile__(
-        "1: \n"
-        "movl $1, %%eax \n"
-        "xchgl %%eax, %0 \n"
-        "testl %%eax, %%eax \n"
-        "jnz 1b \n"
+    __asm(
+        "1: ;"
+        "movl $1, %%eax ;"
+        "xchgl %%eax, %0 ;"
+        "testl %%eax, %%eax ;"
+        "jnz 1b ;"
         : "=m" (lockAllThread)
-        :
-        : "eax"
-        
+        :: "eax"
     );
-    printf("%d", inter);
     printf("%d", lockAllThread);
 };
 
 void unlock(){
     //implement spinlock unlock with inline assempbly
-    __asm__ __volatile__(
-        "movl $0, %0 \n"
+    __asm(
+        "movl $0, %0 ;"
         : "=m" (lockAllThread)
-        :
-        : "eax"
-        
+        :: "eax"
     );
     }
 
