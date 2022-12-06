30f70d6f3099cf979c1c2c1b1df6a170a001d506
GigaUltimatumXVI <adriennotte@hotmail.com>
2022-12-06 19:00:50 +0100

    newActive

---
+++ Philosof _AC.c
@@ -0,0 +1,63 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+int numberOfPhilo;
+pthread_mutex_t *baguette;
+
+void* philosophe (void* arg){
+  int * idl = (int*) arg;
+  int left = *idl;
+  int numberOfPhilo1 = (numberOfPhilo==1) ? numberOfPhilo+1 : numberOfPhilo;
+  int right = (left + 1) % numberOfPhilo1;
+  for (int j = 0; j < 100000; j++) {
+    if(left<right) {
+      pthread_mutex_lock(&baguette[left]);
+      pthread_mutex_lock(&baguette[right]);
+    }
+    else {
+      pthread_mutex_lock(&baguette[right]);
+      pthread_mutex_lock(&baguette[left]);
+    }
+    pthread_mutex_unlock(&baguette[left]);
+    pthread_mutex_unlock(&baguette[right]);
+  }
+  return (NULL);
+}
+
+int main ( int argc, char *argv[]){
+  numberOfPhilo = atoi(argv[1]);
+  int id[numberOfPhilo];
+  int numberOfPhilo1 = (numberOfPhilo==1) ? numberOfPhilo+1 : numberOfPhilo;
+  pthread_t *phil = (pthread_t *) malloc(numberOfPhilo*sizeof(pthread_t));
+
+  //create 2 baguettes if there is only one philo
+  if (numberOfPhilo == 1){
+    baguette = (pthread_mutex_t *) malloc((numberOfPhilo+1) * sizeof(pthread_mutex_t));
+  }
+  else{
+    baguette = (pthread_mutex_t *) malloc(numberOfPhilo * sizeof(pthread_mutex_t));
+  }
+
+  for (int i = 0; i < numberOfPhilo; i++){
+    id[i]=i;
+  }
+  for (int i = 0; i < numberOfPhilo1; i++){
+    if(pthread_mutex_init( &baguette[i], NULL)!=0) return 1;
+  }
+  for (int i = 0; i < numberOfPhilo; i++){
+    if(pthread_create(&phil[i], NULL, philosophe, (void*)&(id[i]) )!=0)return 1;
+  }
+  for (int i = 0; i < numberOfPhilo; i++){
+    if(pthread_join(phil[i], NULL)!=0)return 1;
+  }
+  for (int i = 0; i < numberOfPhilo1; i++){
+    if(pthread_mutex_destroy(&baguette[i])!=0)return 1;
+  }
+
+  free(baguette);
+  free(phil);
+  return 0;
+}
---
+++ WriterReader_AC.c
@@ -0,0 +1,130 @@
+#include<pthread.h>
+#include<semaphore.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+
+pthread_mutex_t mutex_reader;
+pthread_mutex_t mutex_writer;
+pthread_mutex_t z;// Pour être sure que les writer aient tjrs la prioritée
+sem_t db_reader; // accès à la db
+sem_t db_writer; // accès à la db
+int writercount = 0;
+int readcount=0; // nombre de readers
+int reading = 0;
+int writing = 0;
+
+void write_database(){
+    for (int i=0; i<10000; i++);
+}
+void read_database(){
+    for (int i=0; i<10000; i++);
+}
+
+void* writer(){
+    int run = 1;
+while(run)
+    {
+    pthread_mutex_lock(&mutex_writer);
+    writercount ++;
+    if(writercount == 1){
+        sem_wait(&db_reader);
+    }
+    pthread_mutex_unlock(&mutex_writer);
+    
+    sem_wait(&db_writer);
+
+    // section critique, un seul writer à la fois
+    if(writing < 2560){
+        writing ++;
+        write_database();
+    }
+    else{
+        run = 0;
+    }
+    sem_post(&db_writer);
+
+    pthread_mutex_lock(&mutex_writer);
+    writercount --;
+    if(writercount == 0){
+        sem_post(&db_reader);
+    }
+    pthread_mutex_unlock(&mutex_writer);
+    }
+}
+
+void* reader()
+{
+    int run = 1;
+    while(run)
+    {
+        pthread_mutex_lock(&z);
+        sem_wait(&db_reader);
+        pthread_mutex_lock(&mutex_reader);
+        // section critique
+        readcount++;
+        if (readcount==1)
+        { // arrivée du premier reader
+            sem_wait(&db_writer);
+        }
+        pthread_mutex_unlock(&mutex_reader);
+        sem_post(&db_reader);
+        pthread_mutex_lock(&mutex_reader);
+        if(reading < 640){
+            read_database();
+            reading ++;
+            readcount--;
+            if(readcount==0)
+            { // départ du dernier reader
+                sem_post(&db_writer);
+            }
+        }
+        else{
+            run = 0;
+        }
+        pthread_mutex_unlock(&mutex_reader);
+        pthread_mutex_unlock(&z);
+        }
+}
+
+int main(int argc, char const *argv[]){
+    int nreader;
+    int nwriter;
+    sscanf(argv[2], "%d", &nreader);
+    sscanf(argv[1], "%d", &nwriter);
+    sem_init(&db_reader, 0, 1);
+    sem_init(&db_writer, 0, 1);
+    pthread_t ReadThreads[nreader];
+    pthread_t WriteThreads[nwriter];
+    for (size_t i = 0; i < nwriter; i++)
+    {
+        if(0 != pthread_create(&WriteThreads[i],NULL,&writer,NULL)){
+            printf("ERROR: Error while creating writer thread n°%ld\n",i);
+        }
+        //else printf("Writer Thread %ld created\n",i+1);
+    }
+
+    for (size_t i = 0; i < nreader; i++)
+    {
+        if(0 != pthread_create(&ReadThreads[i],NULL,&reader,NULL)){
+            printf("ERROR: Error while creating Reader thread n°%ld",i);
+        }
+        //else printf("Reader Thread %ld created\n",i+1);
+    }
+    
+    for (size_t i = 0; i < nwriter; i++)
+    {
+        if(0 != pthread_join(WriteThreads[i],NULL)){
+            printf("ERROR: Error while joining writer thread n°%ld\n",i);
+        }
+    }
+    for (size_t i = 0; i < nreader; i++)
+    {
+        if(0 != pthread_join(ReadThreads[i],NULL)){
+            printf("ERROR: Error while joining reader thread n°%ld\n",i);
+        }
+    }
+    //printf("%d writing\n",writing);
+    //printf("%d reading\n",reading);
+    return 0;
+}
