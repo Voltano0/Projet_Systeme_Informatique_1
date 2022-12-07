14b6007a4bc96f764d8e43d76018b5c773c79e09
GigaUltimatumXVI <adriennotte@hotmail.com>
2022-12-07 09:44:52 +0100

    TrickDederierelesfagots

--- GraphMaker.py
+++ GraphMaker.py
@@ -17,6 +17,14 @@ for prog in df['Program'].unique():
         MeanTimes.append(newdf[newdf['Threads'] == i]['Temps'].mean())
         StdTimes.append(newdf[newdf['Threads'] == i]['Temps'].std())
     plt.clf()
+    if(list[i] == "Producer_Consumer" or list[i] == "Writer_Reader"):
+        MeanTimes = MeanTimes[:len(MeanTimes)-1]
+        StdTimes = StdTimes[:len(StdTimes)-1]
+        RawTimes = RawTimes[:len(RawTimes)-1]
+        MeanTimes.insert(1,MeanTimes[0])
+        StdTimes.insert(1,StdTimes[0])
+        RawTimes.insert(1,RawTimes[0])
+
     plt.boxplot(RawTimes, labels=Nthreads,showmeans=True)
     #plt.errorbar(Nthreads, MeanTimes, yerr=StdTimes, fmt='o')
     plt.ylim(ymin=0)
@@ -43,10 +51,20 @@ for i in range(0,df['Program'].unique().size,2):
         StdTimes2.append(newdf2[newdf2['Threads'] == j]['Temps'].std())
     plt.clf()
     #plt.boxplot(RawTimes, labels=Nthreads,showmeans=True)
-    plt.errorbar(Nthreads, MeanTimes1, yerr=StdTimes1, label=list[i],fmt='o')
-    plt.errorbar(Nthreads, MeanTimes2, yerr=StdTimes2, label=list[i+1],fmt='o')
+    if(list[i] == "Producer_Consumer" or list[i] == "WriterReader"):
+        MeanTimes1 = MeanTimes1[:len(MeanTimes1)-1]
+        MeanTimes2 = MeanTimes2[:len(MeanTimes2)-1]
+        StdTimes1 = StdTimes1[:len(StdTimes1)-1]
+        StdTimes2 = StdTimes2[:len(StdTimes2)-1]
+        MeanTimes1.insert(1,MeanTimes1[0])
+        MeanTimes2.insert(1,MeanTimes2[0])
+        StdTimes1.insert(1,StdTimes1[0])
+        StdTimes2.insert(1,StdTimes2[0])
+    plt.errorbar(Nthreads, MeanTimes1, yerr=StdTimes1, label=list[i])
+    plt.errorbar(Nthreads, MeanTimes2, yerr=StdTimes2, label=list[i+1])
     plt.ylim(ymin=0)
     plt.xlabel('Number of threads')
+    plt.xticks(Nthreads)
     plt.ylabel('Time (s)')
     plt.title(f"{list[i]} and {list[i+1]}")
     plt.legend()
