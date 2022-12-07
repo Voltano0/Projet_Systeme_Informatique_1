3a881c0695654b516af0cc9e1a6c41065d1348d4
GigaUltimatumXVI <adriennotte@hotmail.com>
2022-12-07 11:56:11 +0100

    ReOrganiseExp

--- experiments.sh
+++ experiments.sh
@@ -6,6 +6,11 @@ for i in 1 2 4 8 16 32 64;do
         echo "Philosof;$i;$j;$(/usr/bin/time -f %e temp/Philosof $i --quiet 2>&1)"
     done
 done
+for i in 1 2 4 8 16 32 64;do
+    for j in $(seq 1 5);do
+        echo "Philosof_AC;$i;$j;$(/usr/bin/time -f %e temp/Philosof_AC $i --quiet 2>&1)"
+    done
+done
 for i in 1 2 4 8 16 32;do
     for j in $(seq 1 5);do
         echo "Producer_Consumer;$i;$j;$(/usr/bin/time -f %e temp/Producer_Consumer $i $i --quiet 2>&1)"
@@ -13,7 +18,17 @@ for i in 1 2 4 8 16 32;do
 done
 for i in 1 2 4 8 16 32;do
     for j in $(seq 1 5);do
+        echo "Producer_Consumer_AC;$i;$j;$(/usr/bin/time -f %e temp/Producer_Consumer_AC $i $i --quiet 2>&1)"
+    done
+done
+for i in 1 2 4 8 16 32;do
+    for j in $(seq 1 5);do
         echo "WriterReader;$i;$j;$(/usr/bin/time -f %e temp/WriterReader $i $i --quiet 2>&1)"
+    done
+done
+for i in 1 2 4 8 16 32;do
+    for j in $(seq 1 5);do
+        echo "WriterReader_AC;$i;$j;$(/usr/bin/time -f %e temp/WriterReader_AC $i $i --quiet 2>&1)"
     done
 done
 for i in 1 2 4 8 16 32 64;do
@@ -24,20 +39,5 @@ done
 for i in 1 2 4 8 16 32 64;do
     for j in $(seq 1 5);do
         echo "Test-and-test-and-set;$i;$j;$(/usr/bin/time -f %e temp/Perftest2 $i --quiet 2>&1)"
-    done
-done
-for i in 1 2 4 8 16 32 64;do
-    for j in $(seq 1 5);do
-        echo "Philosof_AC;$i;$j;$(/usr/bin/time -f %e temp/Philosof_AC $i --quiet 2>&1)"
-    done
-done
-for i in 1 2 4 8 16 32;do
-    for j in $(seq 1 5);do
-        echo "Producer_Consumer_AC;$i;$j;$(/usr/bin/time -f %e temp/Producer_Consumer_AC $i $i --quiet 2>&1)"
-    done
-done
-for i in 1 2 4 8 16 32;do
-    for j in $(seq 1 5);do
-        echo "WriterReader_AC;$i;$j;$(/usr/bin/time -f %e temp/WriterReader_AC $i $i --quiet 2>&1)"
     done
 done
