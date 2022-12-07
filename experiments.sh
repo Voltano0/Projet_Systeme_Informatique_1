82f1f1739dedaa81d07b6173f3895b64314714e4
GigaUltimatumXVI <adriennotte@hotmail.com>
2022-12-07 05:08:04 +0100

    reparexperiment

--- experiments.sh
+++ experiments.sh
@@ -1,7 +1,41 @@
 #!/usr/bin/bash
 echo "Program;Threads;Test;Temps"
 
-
+for i in 1 2 4 8 16 32 64;do
+    for j in $(seq 1 5);do
+        echo "Philosof;$i;$j;$(/usr/bin/time -f %e temp/Philosof $i --quiet 2>&1)"
+    done
+done
+for i in 1 2 4 8 16 32 64;do
+    for j in $(seq 1 5);do
+        echo "Producer_Consumer;$i;$j;$(/usr/bin/time -f %e temp/Producer_Consumer $i $i --quiet 2>&1)"
+    done
+done
+for i in 1 2 4 8 16 32 64;do
+    for j in $(seq 1 5);do
+        echo "WriterReader;$i;$j;$(/usr/bin/time -f %e temp/WriterReader $i $i --quiet 2>&1)"
+    done
+done
+for i in 1 2 4 8 16 32 64;do
+    for j in $(seq 1 5);do
+        echo "Test-and-set;$i;$j;$(/usr/bin/time -f %e temp/Perftest $i --quiet 2>&1)"
+    done
+done
+for i in 1 2 4 8 16 32 64;do
+    for j in $(seq 1 5);do
+        echo "Test-and-test-and-set;$i;$j;$(/usr/bin/time -f %e temp/Perftest2 $i --quiet 2>&1)"
+    done
+done
+for i in 1 2 4 8 16 32 64;do
+    for j in $(seq 1 5);do
+        echo "Philosof_AC;$i;$j;$(/usr/bin/time -f %e temp/Philosof_AC $i --quiet 2>&1)"
+    done
+done
+for i in 1 2 4 8 16 32 64;do
+    for j in $(seq 1 5);do
+        echo "Producer_Consumer_AC;$i;$j;$(/usr/bin/time -f %e temp/Producer_Consumer_AC $i $i --quiet 2>&1)"
+    done
+done
 for i in 1 2 4 8 16 32 64;do
     for j in $(seq 1 5);do
         echo "WriterReader_AC;$i;$j;$(/usr/bin/time -f %e temp/WriterReader_AC $i $i --quiet 2>&1)"
